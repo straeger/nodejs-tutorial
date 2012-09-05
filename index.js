7528382839bf0ef9ffa8103dcb1932c6f0524202
Stephan <stephan.traeger@dff-solutions.de>
2012-09-05 11:38:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var server = require("./server");
+var router = require("./router");
+var requesthandlers = require("./requesthandlers");
+
+var handle ={};
+handle['/'] = requesthandlers.start;
+handle['/start'] = requesthandlers.start;
+handle['/upload'] = requesthandlers.upload;
+handle["/show"] = requesthandlers.show;
+
+server.start(router.route, handle);
